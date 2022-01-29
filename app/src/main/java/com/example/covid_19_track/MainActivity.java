b1fb583e53df988e2aff88a9f8be1c62982933a2
prerana <prerana.22020235@viit.ac.in>
2022-01-29 19:04:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/covid_19_track/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.covid_19_track;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView logo, slogan;
+
+    private static int SPLASH_SCREEN = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //hooks
+        image = findViewById(R.id.imageView2);
+        logo = findViewById(R.id.textView);
+        slogan = findViewById(R.id.textView2);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,DashBoard.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+    }
+}
